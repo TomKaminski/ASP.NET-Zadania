7cde66e725ffa993f8cfcc8fd4ba5fcc03d4b660
TomKaminski <tkaminski93@gmail.com>
2015-03-03 18:06:32 +0100

    SSADCXSA

---
+++ EF.Test/Entities/Member.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EF.Test.Entities
+{
+    public enum EMemberTitle
+    {
+        Developer,
+        TeamLeader,
+        ScrumMaster,
+        Tester
+    }
+    public class Member
+    {
+        public Member()
+        {
+            Projects = new List<Project>();
+        }
+        public int Id { get; set; }
+        public virtual ICollection<Project> Projects { get; set; }
+        public string Name { get; set; }
+        public EMemberTitle MemberTitle { get; set; }
+        [ForeignKey("TeamId")]
+        public virtual Team Team { get; set; }
+
+        
+        public int? TeamId { get; set; }
+    }
+}
